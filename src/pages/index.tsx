c153e06bcfcd25352141aa1050d1f3eb53e4d970
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-12-12 15:00:27 +0000

    More styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -121,209 +121,235 @@ export default function Home() {
     <div className="app">
       <form onSubmit={fetchApi} style={{ width: '100%', height: '100%' }}>
         <div className="app-container">
-          <Typography variant="h2">RESTORIX</Typography>
-          <br />
-          <Box
-            sx={{
-              backgroundColor: '#C9E3AC',
-              // color: 'white',
-              width: '-webkit-fill-available',
-              height: 'fit-content',
-              borderRadius: '10px',
-              padding: '25px',
-              marginTop: '10px',
+          <div
+            style={{
               display: 'flex',
-              flexDirection: 'row',
-              position: 'relative',
-              alignItems: 'flex-start',
-              maxWidth: 'fit-content',
+              flexDirection: 'column',
+              width: '90%',
+              maxWidth: '50rem',
+              alignItems: 'center',
+              marginTop: '2rem',
             }}
           >
-            <div
-              style={{
+            <Typography variant="h2">RESTORIX</Typography>
+            <br />
+            <br />
+            <Box
+              sx={{
+                backgroundColor: '#463940',
+                // color: 'white',
+                width: '-webkit-fill-available',
+                height: 'fit-content',
+                borderRadius: '10px',
+                padding: '25px',
+                marginTop: '10px',
                 display: 'flex',
-                flexDirection: 'column',
-                margin: '0 1rem 0 1rem',
+                flexDirection: 'row',
+                position: 'relative',
+                alignItems: 'flex-start',
+                justifyContent: 'space-around',
               }}
             >
-              <Typography variant="h6">Settings</Typography>
-              <br />
-
-              <TextField
-                type="text"
-                id="backup_path"
-                name="backup_path"
-                required
-                label={`Backup location (absolute path) ${
-                  useSSH ? '(Using SSH!)' : null
-                }`}
-                variant="standard"
-              />
-
-              <br />
-
-              <TextField
-                type="text"
-                id="crontab_env"
-                name="crontab_env"
-                label="Crontab"
-                variant="standard"
-              />
-              <br />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    id="use_ssh"
-                    name="use_ssh"
-                    value="use_ssh"
-                    onChange={() => setUseSSH(!useSSH)}
-                  />
-                }
-                label="Use SSH"
-              />
-
-              {useSSH ? (
-                <>
-                  <br />
-                  <TextField
-                    type="text"
-                    id="ssh_host"
-                    name="ssh_settings"
-                    label="Host"
-                    variant="standard"
-                  />
-                  <br />
-                  <TextField
-                    type="text"
-                    id="ssh_username"
-                    name="ssh_settings"
-                    label="Username"
-                    variant="standard"
-                  />
-                  <br />
-                  <TextField
-                    type="password"
-                    id="ssh_password"
-                    name="ssh_settings"
-                    label="Password"
-                    variant="standard"
-                  />
-                  <br />
-                </>
-              ) : null}
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                margin: '0 1rem 0 1rem',
-              }}
-            >
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  width: '-webkit-fill-available',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <RadioGroup
-                  row
-                  aria-labelledby="demo-row-radio-buttons-group-label"
-                  name="row-radio-buttons-group"
-                  onChange={(data: any) => {
-                    setModeEnv(data.target.value);
-                  }}
-                >
-                  <FormControlLabel
-                    control={<Radio />}
-                    label="Backup"
-                    id="backup"
-                    value="backup"
-                    checked={modeEnv === 'backup'}
-                  />
-                  <FormControlLabel
-                    value="restore"
-                    control={<Radio />}
-                    label="Restore"
-                    id="restore"
-                    checked={modeEnv === 'restore'}
-                  />
-                </RadioGroup>
-              </div>
-              <br />
-              <Button type="submit" variant="contained">
-                START RESTORIX
-              </Button>
-              {containerStatus === 'running' && (
-                <>
-                  <br />
-                  <Button
-                    onClick={() => {
-                      stopContainer();
-                    }}
-                    variant="contained"
-                    color="error"
-                  >
-                    STOP RESTORIX
-                  </Button>
-                </>
-              )}
-              <br />
-              <div style={{ height: '2rem' }} />
               <div
                 style={{
                   display: 'flex',
                   flexDirection: 'column',
-                  alignItems: 'center',
+                  margin: '0 1rem 0 1rem',
                 }}
               >
-                <Typography variant="h6">Service Status:</Typography>
+                <Typography variant="h6">Settings</Typography>
+                <br />
 
-                <Typography
-                  style={{
-                    color:
-                      containerStatus === 'running'
-                        ? 'green'
-                        : containerStatus === 'inexistent'
-                        ? 'red'
-                        : 'black',
-                  }}
-                  variant="body1"
-                >
-                  {containerStatus === 'running' ? (
-                    <>Running</>
-                  ) : containerStatus === 'inexistent' ? (
-                    <>Not running</>
-                  ) : (
-                    <>{containerStatus}</>
-                  )}
-                </Typography>
+                <TextField
+                  type="text"
+                  id="backup_path"
+                  name="backup_path"
+                  required
+                  label={`Backup location (absolute path) ${
+                    useSSH ? '(Using SSH!)' : null
+                  }`}
+                  variant="standard"
+                />
+
+                <br />
+
+                <TextField
+                  type="text"
+                  id="crontab_env"
+                  name="crontab_env"
+                  label="Crontab"
+                  variant="standard"
+                />
+                <br />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      id="use_ssh"
+                      name="use_ssh"
+                      value="use_ssh"
+                      onChange={() => setUseSSH(!useSSH)}
+                    />
+                  }
+                  label="Use SSH"
+                />
+
+                {useSSH ? (
+                  <>
+                    <br />
+                    <TextField
+                      type="text"
+                      id="ssh_host"
+                      name="ssh_settings"
+                      label="Host"
+                      variant="standard"
+                    />
+                    <br />
+                    <TextField
+                      type="text"
+                      id="ssh_username"
+                      name="ssh_settings"
+                      label="Username"
+                      variant="standard"
+                    />
+                    <br />
+                    <TextField
+                      type="password"
+                      id="ssh_password"
+                      name="ssh_settings"
+                      label="Password"
+                      variant="standard"
+                    />
+                    <br />
+                  </>
+                ) : null}
               </div>
-            </div>
-          </Box>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <br />
-            <Typography variant="h6">Volume List:</Typography>
-            {volumeList.map((volume: any) => {
-              return (
-                <div key={volume.Name}>
-                  <br />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        id={volume.Name}
-                        name="selected_volumes"
-                        value={volume.Name}
-                        size="small"
-                      />
-                    }
-                    label={volume.Name}
-                  />
-                  <br />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  margin: '0 1rem 0 1rem',
+                }}
+              >
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    width: '-webkit-fill-available',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                    onChange={(data: any) => {
+                      setModeEnv(data.target.value);
+                    }}
+                  >
+                    <FormControlLabel
+                      control={<Radio />}
+                      label="Backup"
+                      id="backup"
+                      value="backup"
+                      checked={modeEnv === 'backup'}
+                    />
+                    <FormControlLabel
+                      value="restore"
+                      control={<Radio />}
+                      label="Restore"
+                      id="restore"
+                      checked={modeEnv === 'restore'}
+                    />
+                  </RadioGroup>
                 </div>
-              );
-            })}
+                <br />
+                <Button type="submit" variant="contained">
+                  START RESTORIX
+                </Button>
+                {containerStatus === 'running' && (
+                  <>
+                    <br />
+                    <Button
+                      onClick={() => {
+                        stopContainer();
+                      }}
+                      variant="contained"
+                      color="error"
+                    >
+                      STOP RESTORIX
+                    </Button>
+                  </>
+                )}
+                <br />
+                <div style={{ height: '2rem' }} />
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Typography variant="h6">Service Status:</Typography>
+
+                  <Typography
+                    style={{
+                      color:
+                        containerStatus === 'running'
+                          ? 'green'
+                          : containerStatus === 'inexistent'
+                          ? 'red'
+                          : 'black',
+                    }}
+                    variant="body1"
+                  >
+                    {containerStatus === 'running' ? (
+                      <>Running</>
+                    ) : containerStatus === 'inexistent' ? (
+                      <>Not running</>
+                    ) : (
+                      <>{containerStatus}</>
+                    )}
+                  </Typography>
+                </div>
+              </div>
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: '#463940',
+                // color: 'white',
+                width: '-webkit-fill-available',
+                height: 'fit-content',
+                borderRadius: '10px',
+                padding: '2rem',
+                marginTop: '2rem',
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'relative',
+                alignItems: 'flex-start',
+                justifyContent: 'space-around',
+              }}
+            >
+              <Typography variant="h6">Volume List:</Typography>
+              {volumeList.map((volume: any) => {
+                return (
+                  <div key={volume.Name}>
+                    <br />
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          id={volume.Name}
+                          name="selected_volumes"
+                          value={volume.Name}
+                          size="small"
+                        />
+                      }
+                      label={volume.Name}
+                    />
+                    <br />
+                  </div>
+                );
+              })}
+            </Box>
           </div>
         </div>
       </form>
