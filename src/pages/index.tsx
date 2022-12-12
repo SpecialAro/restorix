c15e5f481806d6037b9fba44a97dc12547fff736
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-12-12 14:24:55 +0000

    Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Switch,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { API_BASEURL } from '../config';
-import styles from '../styles/Home.module.css';
 
 function stopContainer() {
   fetch(`${API_BASEURL}/container/stop`, {
@@ -13,6 +23,7 @@ function stopContainer() {
 
 export default function Home() {
   const [useSSH, setUseSSH] = useState(false);
+  const [modeEnv, setModeEnv] = useState<string>('backup');
   const [volumeList, setVolumeList] = useState([]);
   const [containerStatus, setContainerStatus] = useState<string | undefined>(
     undefined,
@@ -21,7 +32,6 @@ export default function Home() {
   async function fetchApi(event: any) {
     event.preventDefault();
     const backupPath = event.target.backup_path.value;
-    const modeEnv = event.target.mode_env.value;
     const crontabEnv = event.target.crontab_env.value;
 
     const sshHost = useSSH ? event.target.ssh_host.value : undefined;
@@ -108,118 +118,215 @@ export default function Home() {
   });
 
   return (
-    <div className={styles.container}>
-      <form onSubmit={fetchApi}>
-        <label>
-          Backup location (absolute path) {useSSH ? '(Using SSH!)' : null}
+    <div className="app">
+      <form onSubmit={fetchApi} style={{ width: '100%', height: '100%' }}>
+        <div className="app-container">
+          <Typography variant="h2">RESTORIX</Typography>
           <br />
-          <input type="text" id="backup_path" name="backup_path" required />
-        </label>
-        <br />
-        <br />
-        <label>
-          Crontab
-          <br />
-          <input type="text" id="crontab_env" name="crontab_env" />
-        </label>
-        <br />
-        <br />
-        {volumeList.map((volume: any) => {
-          return (
-            <div key={volume.Name}>
-              <label>
-                <input
-                  type="checkbox"
-                  id={volume.Name}
-                  name="selected_volumes"
-                  value={volume.Name}
-                />
-                {volume.Name}
-              </label>
-            </div>
-          );
-        })}
-        <br />
-        <label>
-          Use SSH
-          <input
-            type="checkbox"
-            id="use_ssh"
-            name="use_ssh"
-            value="use_ssh"
-            onChange={() => setUseSSH(!useSSH)}
-          />
-        </label>
+          <Box
+            sx={{
+              backgroundColor: '#C9E3AC',
+              // color: 'white',
+              width: '-webkit-fill-available',
+              height: 'fit-content',
+              borderRadius: '10px',
+              padding: '25px',
+              marginTop: '10px',
+              display: 'flex',
+              flexDirection: 'row',
+              position: 'relative',
+              alignItems: 'flex-start',
+              maxWidth: 'fit-content',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: '0 1rem 0 1rem',
+              }}
+            >
+              <Typography variant="h6">Settings</Typography>
+              <br />
 
-        {useSSH ? (
-          <>
-            <br />
-            <br />
-            <label>
-              Host
+              <TextField
+                type="text"
+                id="backup_path"
+                name="backup_path"
+                required
+                label={`Backup location (absolute path) ${
+                  useSSH ? '(Using SSH!)' : null
+                }`}
+                variant="standard"
+              />
+
               <br />
-              <input type="text" id="ssh_host" name="ssh_settings" />
-            </label>
-            <br />
-            <label>
-              Username
+
+              <TextField
+                type="text"
+                id="crontab_env"
+                name="crontab_env"
+                label="Crontab"
+                variant="standard"
+              />
               <br />
-              <input type="text" id="ssh_username" name="ssh_settings" />
-            </label>
-            <br />
-            <label>
-              Password
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    id="use_ssh"
+                    name="use_ssh"
+                    value="use_ssh"
+                    onChange={() => setUseSSH(!useSSH)}
+                  />
+                }
+                label="Use SSH"
+              />
+
+              {useSSH ? (
+                <>
+                  <br />
+                  <TextField
+                    type="text"
+                    id="ssh_host"
+                    name="ssh_settings"
+                    label="Host"
+                    variant="standard"
+                  />
+                  <br />
+                  <TextField
+                    type="text"
+                    id="ssh_username"
+                    name="ssh_settings"
+                    label="Username"
+                    variant="standard"
+                  />
+                  <br />
+                  <TextField
+                    type="password"
+                    id="ssh_password"
+                    name="ssh_settings"
+                    label="Password"
+                    variant="standard"
+                  />
+                  <br />
+                </>
+              ) : null}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: '0 1rem 0 1rem',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  width: '-webkit-fill-available',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                  onChange={(data: any) => {
+                    setModeEnv(data.target.value);
+                  }}
+                >
+                  <FormControlLabel
+                    control={<Radio />}
+                    label="Backup"
+                    id="backup"
+                    value="backup"
+                    checked={modeEnv === 'backup'}
+                  />
+                  <FormControlLabel
+                    value="restore"
+                    control={<Radio />}
+                    label="Restore"
+                    id="restore"
+                    checked={modeEnv === 'restore'}
+                  />
+                </RadioGroup>
+              </div>
               <br />
-              <input type="password" id="ssh_password" name="ssh_settings" />
-            </label>
-          </>
-        ) : null}
-        <br />
-        <br />
-        <label>
-          <input
-            type="radio"
-            id="backup"
-            name="mode_env"
-            value="backup"
-            checked
-          />
-          Backup
-        </label>
-        <br />
-        <label>
-          <input type="radio" id="restore" name="mode_env" value="restore" />
-          Restore
-        </label>
-        <br />
-        <br />
-        <button type="submit">START RESTORIX</button>
+              <Button type="submit" variant="contained">
+                START RESTORIX
+              </Button>
+              {containerStatus === 'running' && (
+                <>
+                  <br />
+                  <Button
+                    onClick={() => {
+                      stopContainer();
+                    }}
+                    variant="contained"
+                    color="error"
+                  >
+                    STOP RESTORIX
+                  </Button>
+                </>
+              )}
+              <br />
+              <div style={{ height: '2rem' }} />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Typography variant="h6">Service Status:</Typography>
+
+                <Typography
+                  style={{
+                    color:
+                      containerStatus === 'running'
+                        ? 'green'
+                        : containerStatus === 'inexistent'
+                        ? 'red'
+                        : 'black',
+                  }}
+                  variant="body1"
+                >
+                  {containerStatus === 'running' ? (
+                    <>Running</>
+                  ) : containerStatus === 'inexistent' ? (
+                    <>Not running</>
+                  ) : (
+                    <>{containerStatus}</>
+                  )}
+                </Typography>
+              </div>
+            </div>
+          </Box>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <br />
+            <Typography variant="h6">Volume List:</Typography>
+            {volumeList.map((volume: any) => {
+              return (
+                <div key={volume.Name}>
+                  <br />
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        id={volume.Name}
+                        name="selected_volumes"
+                        value={volume.Name}
+                        size="small"
+                      />
+                    }
+                    label={volume.Name}
+                  />
+                  <br />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </form>
-      <br />
-      {containerStatus === 'running' && (
-        <button
-          onClick={() => {
-            stopContainer();
-          }}
-        >
-          STOP RESTORIX
-        </button>
-      )}
-      <br />
-      <br />
-      Service Status:{' '}
-      <span
-        style={{
-          color:
-            containerStatus === 'running'
-              ? 'green'
-              : containerStatus === 'inexistent'
-              ? 'red'
-              : 'black',
-        }}
-      >
-        {containerStatus}
-      </span>
     </div>
   );
 }
