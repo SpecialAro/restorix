9daaaa7977e84c1b2ad31349bcef6c704c5203d2
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-12-14 17:48:09 +0000

    Set default SERVER_BASEURL env var

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ import getConfig from 'next/config';
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
-export const SERVER_BASEURL = publicRuntimeConfig.SERVER_BASEURL;
+export const SERVER_BASEURL =
+  publicRuntimeConfig.SERVER_BASEURL || 'http://localhost:3000';
 export const API_BASEURL = `${SERVER_BASEURL}/api`;
